e847b7027efa39e18ea6d264d46284678595db7d
masterc18 <carlos.talavera16038@est.uca.edu.ni>
2023-05-18 15:45:39 -0600

    Se realizo el ejercicio 4

---
+++ Ejercicio4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+void NumeroPrimo()
+{
+    int num;
+    cout << "Dime un numero " << endl;
+    cin >> num;
+    if (num % num == 0)
+    {
+        cout << "El numero es primo " << endl;
+    }
+    else
+    {
+        cout << "No es numero primo " << endl;
+    }
+}
+
+int main()
+{
+    NumeroPrimo();
+    return 0;
+}
